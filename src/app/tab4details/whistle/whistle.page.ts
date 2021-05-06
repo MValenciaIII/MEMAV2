77376185746933000af27d422dfcc3e32a7b21c9
Charllieya <charllieya@gmail.com>
2021-05-06 13:20:55 -0500

    whistle page

---
+++ src/app/tab4details/whistle/whistle.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-whistle',
+  templateUrl: './whistle.page.html',
+  styleUrls: ['./whistle.page.scss'],
+})
+export class WhistlePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
