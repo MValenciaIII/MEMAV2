cac0c7863bdb41a6d5a62a02d8960c05260761b1
Charlllieya Smith <charllieya@gmail.com>
2021-05-17 14:43:14 -0500

    edits on gps locator

--- src/app/tab4details/gps-location/gps-location-routing.module.ts
+++ src/app/tab4details/gps-location/gps-location-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AgmCoreModule } from '@agm/core';
 
 import { GpsLocationPage } from './gps-location.page';
 
