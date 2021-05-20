514e5c486fd74ce4bed527f2dca96228c1ba05f8
Will <willkelly9617@gmail.com>
2021-05-20 14:56:05 -0500

    Delete contacts button created

--- src/app/tab2details/contacts/contacts.page.ts
+++ src/app/tab2details/contacts/contacts.page.ts
@@ -24,31 +24,12 @@ export class ContactsPage {
     // todo: route to an "id" page?
   }
 
-  DeleteContact(phone) {
-    
-  //   // todo: filter local storage contacts array against ID
-  //   var storedNames = JSON.parse(localStorage.getItem("CONTACTS"));
-  //   console.log("Stored Names" + storedNames)
+  DeleteContact(event) {
 
-  //   // here you need to make a loop to find the index of item to delete
-  //   var indexToRemove = 1;
-
-  //   //remove item selected, second parameter is the number of items to delete 
-  //   storedNames.slice(indexToRemove, 1);
-
-  //  // Put the object into storage
-  //  localStorage.setItem('CONTACTS', JSON.stringify(storedNames));
- 
-    var bookmarks = JSON.parse(localStorage.getItem("CONTACTS"));
-    // console.log(i)
-    console.clear();
-    console.log("Before splice");
-    // console.log(JSON.stringify(bookmarks));
-    console.log((bookmarks));
-    bookmarks.splice(bookmarks);
-    console.log("After splice");
-    console.log(JSON.stringify(bookmarks));
-    localStorage.setItem("CONTACTS", JSON.stringify(bookmarks));
+    let id = event.target.dataset.id; 
+    let newArray = this.contacts.filter(card => card.id != id);
+    localStorage.setItem("CONTACTS", JSON.stringify(newArray));
+    window.location.reload();
 
   }
 
