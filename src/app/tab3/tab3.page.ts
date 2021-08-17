32043879f3f2124bee26539a03baa6b6982144a8
Margarito Valencia III <52177176+MValenciaIII@users.noreply.github.com>
2021-08-17 14:47:30 -0500

    Radius UPdate
    I finally gotten the radius to update without creating more radius's

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgZone, SimpleChange, SimpleChanges, Input } from '@angular/core';
 // import * as Leaflet from 'leaflet';
 import { HttpClient } from "@angular/common/http";
 import * as L from "leaflet";
@@ -17,15 +17,16 @@ import {latLng, MapOptions, tileLayer, Map, Marker, icon} from 'leaflet';
 //update noaa warnings if bad weather near by.
 //also fix puzzle pieced map on load
 export class Tab3Page {
-
+  rangeId: any;
   map: Map;
   mapOptions: MapOptions;
-
+   latitudeOne: any;
+   longitudeOne: any;
+   geoRadiusLine: any;
   
  
   constructor(private http: HttpClient ) {
-
-
+    var Functionone;
     
   }
 
@@ -38,23 +39,26 @@ export class Tab3Page {
 
   ngOnInit(){
 
+    this.getLocationService() 
+    this.initializeMapOptions()
 
-  this.getLocationService()
-  this.initializeMapOptions()
-  
-        
-    }      
+  }   
+    
+    ngOnChange(changes: SimpleChanges){
+    }
 
 
     getLocationService():Promise<any>{
       return new Promise((resolve, reject)=>{
         navigator.geolocation.getCurrentPosition(resp=>{
           resolve({lng: resp.coords.longitude, lat: resp.coords.latitude})
+          this.latitudeOne = resp.coords.latitude
+          this.longitudeOne = resp.coords.longitude
         })
   
       })
     }
-  
+  //MAYBE START STORING VALUES OF RESP.LAT/LNG AND USE FOR CHECKING LAYER THEN REMOVEING IN ONMAPREADY
   
     
     onMapReady(map: Map) {
@@ -62,26 +66,10 @@ export class Tab3Page {
       this.getLocationService().then(resp=> {
         this.map.setView(L.latLng(resp.lat, resp.lng), 18)
       })
+      this.geoRadius(this.rangeId)
       this.getAlerts();
-      this.addSampleMarker();
-      this.geoRadius();
     }
 
-//document.getElementById('radius').addEventListener('input', (event) => { this.changeRadius});
-
-// changeRadius(event) {
-//   var newRadius = event.target.value;
-
-//   this.group.eachLayer(function(layer) {
-//     if (layer instanceof L.Circle) {
-//       layer.setRadius(newRadius);
-//     }
-//   });
-// }
-      // circle = L.circle([32.302898, -90.183487], {
-      //   radius: 1000,
-      // }).addTo(this.group);
-      // marker = L.marker([32.302898, -90.183487]).addTo(this.group);
 
 //MISSISSIPPI AREA
     private initializeMapOptions() {
@@ -105,20 +93,30 @@ export class Tab3Page {
 //UPDATING A MAP RADIUS USING BUTTONS IN ANGULAR
 //ANGULAR BUTTON 
 
-    private geoRadius() {
+    geoRadius(value) {
+      //BUTTON RADIUS: REMOVE PREVIOUS LAYER WHEN NEW BUTTON IS PRESSED
+      //SLIDER RADIUS: LEARN HOW TO UPDATE A RADIUS IMMEDIATELY WHEN RADIUS SLIDER IS MOVED
+      //ALSO HOW TO SAVE RADIUS VALUE.
       this.getLocationService().then(resp=>{
-         const radius =  L.circle([resp.lat, resp.lng], 
-          {
-            radius: 200
-          }).addTo(this.map)
-      })
       
+          this.rangeId = value
+
+        
+        if(this.rangeId === undefined) {
+
+        this.geoRadiusLine = L.circle([resp.lat, resp.lng], { radius: 200}).addTo(this.map);
+        } 
+          else if (this.rangeId > 0) {
+            
+            this.geoRadiusLine.setRadius(this.rangeId)
+          
+          }
+      })
     }
 
-     radiusChange(value :any)  {
-       console.log(value)
-      const radius = L.circle;
-    }
+
+
+
 
 
     private addSampleMarker() {
@@ -140,10 +138,22 @@ export class Tab3Page {
 
 //WEATHER POLOYON 
     private async getAlerts() {
-        let response = this.http.get("https://api.weather.gov/alerts/active?area=MS").subscribe((json: any) => {
+        let response = this.http.get("https://api.weather.gov/alerts/active?area=TN").subscribe((json: any) => {
           console.log(json);
           this.json = json;
-          L.geoJSON(this.json).addTo(this.map);
+          for (let i = 0; i < this.json.features.length; i++) {
+            const element = this.json.features[i];
+            console.log(element)
+            
+            //I NEED TO PARSE THE POLYGONS IN THE RESPONSE INTO POSITIOSN, PATHOPTIONS, KEY
+            //
+            
+            var pressOne = L.geoJSON(element).addTo(this.map);
+            pressOne.bindPopup(`<p>County Affected: ${element.properties.areaDesc}</p>
+            <p>Event: ${element.properties.event}</p>
+            `)
+          }
+          
         });
       }
   }
