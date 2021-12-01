ca3ae9fdd77e5c4b635ca3c2f8ccc0fdacdc2c8f
Margarito Valencia III <52177176+MValenciaIII@users.noreply.github.com>
2021-12-01 16:13:12 -0600

    Home button links
    I've changed couple of the links on the Tab1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouteReuseStrategy } from "@angular/router";
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClient, HttpClientModule} from '@angular/common/http';
 import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
 import { SplashScreen } from "@ionic-native/splash-screen/ngx";
 import { StatusBar } from "@ionic-native/status-bar/ngx";
@@ -25,6 +25,7 @@ import {LeafletModule} from "@asymmetrik/ngx-leaflet"
     Flashlight,
     StatusBar,
     SplashScreen,
+    HttpClient,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy, },
   ],
   bootstrap: [AppComponent],
